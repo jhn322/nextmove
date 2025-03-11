7c43824c1c94a344f89bcfe7ff3fee9cc6867a62
jhn322 <johan.soderlund96@gmail.com>
2025-03-11 22:56:54 +0100

    auth

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -721,7 +721,7 @@ const Navbar = () => {
                         ) : (
                           <>
                             <Button
-                              onClick={() => signIn("google")}
+                              onClick={() => signIn("google", "/")}
                               className="h-9 px-4 py-2 inline-flex items-center justify-center rounded-lg"
                             >
                               <LogIn className="mr-2 h-4 w-4" /> Login
