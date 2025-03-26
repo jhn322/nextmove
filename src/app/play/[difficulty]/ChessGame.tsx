7c1be8fcfad9e7adafd0cf558261754a68b4fbc3
jhn322 <johan.soderlund96@gmail.com>
2025-03-26 21:42:24 +0100

    Moved dnd to root

--- src/app/play/[difficulty]/ChessGame.tsx
+++ src/app/play/[difficulty]/ChessGame.tsx
@@ -2,8 +2,6 @@
 
 import { useParams } from "next/navigation";
 import ChessBoard from "@/components/game/board/ChessBoard";
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
 
 export default function ChessGame() {
   const params = useParams();
@@ -11,9 +9,7 @@ export default function ChessGame() {
 
   return (
     <div className="flex flex-col lg:flex-row items-center justify-center gap-8 p-4 min-h-[calc(100vh-4rem)]">
-      <DndProvider backend={HTML5Backend}>
-        <ChessBoard difficulty={difficulty} />
-      </DndProvider>
+      <ChessBoard difficulty={difficulty} />
     </div>
   );
 }
