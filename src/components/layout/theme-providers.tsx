25436a3d53322361536b1a2fe779e6878f0aaf86
jhn322 <johan.soderlund96@gmail.com>
2025-03-27 00:02:03 +0100

    Enhance theme management in Navbar and global styles
    - Introduce new theme categories and corresponding themes in the Navbar component, improving user customization options.
    - Update the ThemeProvider to include additional themes such as Amethyst, Crimson, Jade, and more for a richer user experience.
    - Refactor global CSS to define styles for new themes, ensuring consistent design across the application.
    - Implement dropdown menu for theme selection, enhancing accessibility and usability for theme switching.

--- src/components/layout/theme-providers.tsx
+++ src/components/layout/theme-providers.tsx
@@ -6,5 +6,28 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 type ThemeProviderProps = Parameters<typeof NextThemesProvider>[0];
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  return (
+    <NextThemesProvider
+      themes={[
+        "light",
+        "dark",
+        "system",
+        "amethyst",
+        "crimson",
+        "jade",
+        "amber",
+        "rose",
+        "cyberpunk",
+        "pokemon",
+        "dracula",
+        "fantasy",
+        "midnight",
+        "classic",
+        "comic",
+      ]}
+      {...props}
+    >
+      {children}
+    </NextThemesProvider>
+  );
 }
